3be800f0f36d768bddd30d9a35d5953a7fa32d5e
Gemma Krebs <gemmakrebs@live.com>
2023-07-10 17:55:35 +1200

    small changes, currently broken

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,8 +1,11 @@
+import Nav from './Nav'
+
 function Header() {
   return (
     <>
       <header>
         <h1>Gemma Krebs</h1>
+        <Nav />
       </header>
     </>
   )
---
+++ client/components/Nav.tsx
@@ -0,0 +1,13 @@
+function Nav() {
+  return (
+    <>
+      <nav>
+        <li>github</li>
+        <li>linked in</li>
+        <li>contact</li>
+      </nav>
+    </>
+  )
+}
+
+export default Nav
